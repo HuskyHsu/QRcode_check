a32e03194c8131a638172ff2fe4a8efd6ccbf5ae
HuskyHsu <chessman416@hotmail.com>
2018-03-27 22:53:16 +0800

    init

---
+++ app.js
@@ -0,0 +1,35 @@
+var items = JSON.parse(localStorage.getItem('items')) || [];
+
+ans = [
+    'ZQWWNAwb', 'FstM3d4b', 'O35WLUKM', 'zufNtN43',
+    '596Y14mW', 'fMT3B12v', '72v6orIm', 'IrpLBSPf',
+    'jyG9tSYO', 'Uq1HlS9E', 'pzM1JXqP'
+]
+
+const itemsList = document.querySelector('.plates');
+
+var url_string = window.location.href;
+var url = new URL(url_string);
+var question = url.searchParams.get("which");
+
+items.push(ans.indexOf(question));
+items.sort(function (a, b) {
+    return a - b;
+});
+items = items.filter(function (elem, pos, arr) {
+    return arr.indexOf(elem) == pos && elem > 0;
+});
+
+populateList(items, itemsList);
+localStorage.setItem('items', JSON.stringify(items));
+
+function populateList(plates = [], platesList) {
+    // 使用map搭配join來組成字串，並顯示在html的清單ul中
+    platesList.innerHTML = plates.map((plate, i) => {
+        return `
+        <li>
+          <label>${plate}</label>
+        </li>
+      `;
+    }).join('');
+}
